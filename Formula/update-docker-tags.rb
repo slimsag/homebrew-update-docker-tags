428de9fe402d74f24e4ecf17d6df7cbd68d6333f
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-08 16:09:50 -0700

    Brew formula update for update-docker-tags version v0.7.0

--- Formula/update-docker-tags.rb
+++ Formula/update-docker-tags.rb
@@ -2,16 +2,16 @@
 class UpdateDockerTags < Formula
   desc "CLI to update Docker tag image versions"
   homepage "https://github.com/slimsag/update-docker-tags"
-  version "0.6.1"
+  version "0.7.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/slimsag/update-docker-tags/releases/download/v0.6.1/update-docker-tags_0.6.1_Darwin_x86_64.tar.gz"
-    sha256 "a17dd3acaf104a3c2e23f7e6499eb3ad32551f149fae698febc9cb624df35b0a"
+    url "https://github.com/slimsag/update-docker-tags/releases/download/v0.7.0/update-docker-tags_0.7.0_Darwin_x86_64.tar.gz"
+    sha256 "763e9edabbcc0c894fda47edca49ceedd005fc85c6a76344b8b43a9023ad57a2"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/slimsag/update-docker-tags/releases/download/v0.6.1/update-docker-tags_0.6.1_Linux_x86_64.tar.gz"
-      sha256 "a109c62c07274bac76cf0e0fb688f896458ee46f4129af640bb1c8341bd64c8b"
+      url "https://github.com/slimsag/update-docker-tags/releases/download/v0.7.0/update-docker-tags_0.7.0_Linux_x86_64.tar.gz"
+      sha256 "3256440b4fa668f34f043429311409002a345085a685fe47d845132494900173"
     end
   end
   
